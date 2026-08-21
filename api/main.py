5718d3490d65681a268968a53d703fa8e7c47857
pranavmohantyy <mohantypranav147@gmail.com>
2026-08-20 20:43:08 -0700

    add redis for task queue

--- api/main.py
+++ api/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-import psycopg2
 import os
+import redis
+import psycopg2
 
 app = FastAPI()
 
@@ -13,8 +14,12 @@ cursor = conn.cursor()
 cursor.execute('CREATE TABLE IF NOT EXISTS tasks (id SERIAL PRIMARY KEY, message TEXT)')
 conn.commit()
 
+r = redis.Redis.from_url(os.environ['REDIS_URL'])
+
 @app.post('/tasks')
 def create_task(task: Task):
     cursor.execute('INSERT INTO tasks (message) VALUES (%s) RETURNING id', (task.message,))
     conn.commit()
-    return {'id': cursor.fetchone()[0]}
+    task_id = cursor.fetchone()[0]
+    r.rpush('task_queue', f'{task_id}:{task.message}')
+    return {'id': task_id}
--- worker/worker.py
+++ worker/worker.py
@@ -1,15 +1,17 @@
-import psycopg2
-import time
 import os
+import time
+import redis
+import psycopg2
 
 def process_tasks():
     conn = psycopg2.connect(os.environ['DATABASE_URL'])
     cursor = conn.cursor()
+    r = redis.Redis.from_url(os.environ['REDIS_URL'])
     while True:
-        cursor.execute('SELECT * FROM tasks')
-        tasks = cursor.fetchall()
-        for task in tasks:
-            print(f'Processing task {task[0]}: {task[1]}')
+        task = r.blpop('task_queue')
+        if task:
+            task_id, task_message = task[1].decode('utf-8').split(':', 1)
+            print(f'Processing task {task_id}: {task_message}')
             time.sleep(2)
         time.sleep(5)
 
